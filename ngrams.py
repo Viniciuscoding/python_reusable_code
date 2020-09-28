d42194e4e8350d220a04eda2b1088833f2071f37
Viniciuscoding <vin.idem@gmail.com>
2020-09-27 21:54:29 -0400

    Update ngrams.py

--- ngrams.py
+++ ngrams.py
@@ -1,8 +1,24 @@
 # Building an ngram algorithm for NLP analysis
 
 class ngrams:
-  # text = the text body you want to create ngrams
-  # n = number of ngrams desired
+    # text = the text body you want to create ngrams
+    # n = number of ngrams desired
   
-  def ngram(text, n):
-  
+    def ngram(text, n):
+        tokens = text.split()
+        bucket = {}
+        if n < 1:
+            print("Any number equal or less than 0 is not an ngram.\nPlease add a positive integer number greater than 0 as a valid ngram number.")
+        elif n > len(tokens):
+            print("The number of ngrams is larger than the text body length.\nPlease add a ngram number smaller than your text body length.")
+        else:
+            if len(tokens) < 1:
+                print("empty string is not valid")
+            else:
+                for i in range(len(tokens) - n + 1):
+                    gram = ' '.join(tokens[i:i+n])
+                    if gram in bucket:
+                        bucket[gram] += 1
+                    else:
+                        bucket[gram] = 1
+        return bucket
